e9c2c831a16b17e9c003fef3779f35c45f80aa15
Funnyyanne <lochienpin@gmail.com>
2025-03-26 11:16:16 +0800

    fix bugs:刷新数据

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -6,7 +6,10 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'to_do.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(create: (context) => ToDoProvider(), child: MyApp(),));
+  runApp(ChangeNotifierProvider(
+    create: (context) => ToDoProvider(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,60 +21,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo List ',
       theme: ThemeData(
-       primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
-      home:  TodoListScreen(),
+      home: TodoListScreen(),
     );
   }
 }
 
 class TodoListScreen extends StatelessWidget {
-   TodoListScreen({Key? key}) : super(key: key);
+  TodoListScreen({Key? key}) : super(key: key);
   final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
       ),
       body: Consumer<ToDoProvider>(
-      builder: (context, todoProvider, child) {
-        return AnimatedList(
+        builder: (context, todoProvider, child) {
+          return AnimatedList(
             key: _listKey,
-          initialItemCount: todoProvider.todos.length,
-          itemBuilder: (context, index, animation) {
-            final todo = todoProvider.todos[index];
-            return SizeTransition(
-      sizeFactor: animation,
-      child:  
-             Slidable(key: Key(todo.id),
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              children: [
-                SlidableAction(
-                 backgroundColor: Colors.red,
-                 foregroundColor: Colors.white,
-                 icon:Icons.delete,
-                 label:'Delete',
-                 onPressed:(context){
-                 todoProvider.deleteTodo(todo.id); 
-                },
-               ),]
-    ),child: CheckboxListTile(
-      title: Text(todo.title),
-      value: todo.isDone,
-      onChanged: (value) {
-        todoProvider.toggleTodo(todo.id);
-      },
-    ),
-    ),
-    );
-          },
-        );
-    },
-    ),
+            initialItemCount: todoProvider.todos.length,
+            itemBuilder: (context, index, animation) {
+              final todo = todoProvider.todos[index];
+              return SizeTransition(
+                sizeFactor: animation,
+                child: Slidable(
+                  key: Key(todo.id),
+                  endActionPane:
+                      ActionPane(motion: const ScrollMotion(), children: [
+                    SlidableAction(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: 'Delete',
+                      onPressed: (context) {
+                        todoProvider.deleteTodo(todo.id);
+                      },
+                    ),
+                  ]),
+                  child: CheckboxListTile(
+                    title: Text(todo.title),
+                    value: todo.isDone,
+                    onChanged: (value) {
+                      todoProvider.toggleTodo(todo.id);
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -97,8 +99,14 @@ class TodoListScreen extends StatelessWidget {
                   ElevatedButton(
                     onPressed: () {
                       if (newTodoTitle.isNotEmpty) {
-                    
-                        Provider.of<ToDoProvider>(context, listen: false).addTodo(newTodoTitle);
+                        final provider =
+                            Provider.of<ToDoProvider>(context, listen: false);
+                        provider.addTodo(newTodoTitle);
+
+                        // Notify AnimatedList about the new item
+                        _listKey.currentState
+                            ?.insertItem(provider.todos.length - 1);
+
                         Navigator.pop(context);
                       }
                     },
@@ -112,6 +120,5 @@ class TodoListScreen extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
     );
-
-}
+  }
 }
